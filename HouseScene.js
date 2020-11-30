a9f17ba275c095382edea39dc7514fda00dd54e1
ivanac7 <ivanac7@users.noreply.github.com>
2020-11-29 23:46:53 -0800

    Created HouseScene

--- HouseScene.js
+++ HouseScene.js
@@ -1,27 +1,9 @@
 import {defs, tiny} from './examples/common.js';
-import {Axes_Viewer, Axes_Viewer_Test_Scene} from "./examples/axes-viewer.js"
-import {Collision_Demo, Inertia_Demo} from "./examples/collisions-demo.js"
-import {Many_Lights_Demo} from "./examples/many-lights-demo.js"
-import {Obj_File_Demo} from "./examples/obj-file-demo.js"
-import {Scene_To_Texture_Demo} from "./examples/scene-to-texture-demo.js"
-import {Surfaces_Demo} from "./examples/surfaces-demo.js"
-import {Text_Demo} from "./examples/text-demo.js"
-import {Transforms_Sandbox} from "./examples/transforms-sandbox.js"
-import {Plane} from "./examples/plane.js"
-import {HouseScene} from "./HouseScene.js"
 // Pull these names into this module's scope for convenience:
 const {
     Vector, Vector3, vec, vec3, vec4, color, Matrix, Mat4, Light, Shape, Material, Shader, Texture, Scene,
     Canvas_Widget, Code_Widget, Text_Widget
 } = tiny;
-
-// Now we have loaded everything in the files tiny-graphics.js, tiny-graphics-widgets.js, and common.js.
-// This yielded "tiny", an object wrapping the stuff in the first two files, and "defs" for wrapping all the rest.
-
-// ******************** Extra step only for when executing on a local machine:
-//                      Load any more files in your directory and copy them into "defs."
-//                      (On the web, a server should instead just pack all these as well
-//                      as common.js into one file for you, such as "dependencies.js")
 
 const Minimal_Webgl_Demo = defs.Minimal_Webgl_Demo;
 
@@ -126,33 +108,28 @@ export class Shape_From_File extends Shape {                                   /
     }
 }
 
-Object.assign(defs,
-    {Axes_Viewer, Axes_Viewer_Test_Scene},
-            {Inertia_Demo, Collision_Demo},
-            {Many_Lights_Demo},
-            {Obj_File_Demo},
-            {Scene_To_Texture_Demo},
-            {Surfaces_Demo},
-            {Text_Demo},
-            {Transforms_Sandbox});
-
-// ******************** End extra step
-
-export class House extends Scene {                           // **Obj_File_Demo** show how to load a single 3D model from an OBJ file.
-                                                                     // Detailed model files can be used in place of simpler primitive-based
-                                                                     // shapes to add complexity to a scene.  Simpler primitives in your scene
-                                                                     // can just be thought of as placeholders until you find a model file
-                                                                     // that fits well.  This demo shows the teapot model twice, with one
-                                                                     // teapot showing off the Fake_Bump_Map effect while the other has
-                                                                     // regular texture and Phong lighting.
+export class HouseScene extends Scene {                           // **Obj_File_Demo** show how to load a single 3D model from an OBJ file.
+    // Detailed model files can be used in place of simpler primitive-based
+    // shapes to add complexity to a scene.  Simpler primitives in your scene
+    // can just be thought of as placeholders until you find a model file
+    // that fits well.  This demo shows the teapot model twice, with one
+    // teapot showing off the Fake_Bump_Map effect while the other has
+    // regular texture and Phong lighting.
     constructor() {
         super();
         // Load the model file:
-        this.shapes = {"house": new Shape_From_File("assets/House.obj")};
+        this.shapes = {
+            "house": new Shape_From_File("assets/House.obj"),
+            "ground": new Shape_From_File("assets/Ground.obj"),
+            "bush": new Shape_From_File("assets/Bush.obj"),
+            "leaves": new Shape_From_File("assets/Leaves.obj"),
+            "trunk": new Shape_From_File("assets/Trunk.obj"),
+            "stepstones": new Shape_From_File("assets/StepStones.obj")
+        };
 
         this.materials = {
             house: new Material(new defs.Phong_Shader(),
-                {ambient: .3, diffusity: .5, color: color(1,1,1,1)}),
+                {ambient: .3, diffusity: .5, color: color(1, 1, 1, 1)}),
         };
 
         // Don't create any DOM elements to control this scene:
@@ -169,9 +146,9 @@ export class House extends Scene {                           // **Obj_File_Demo*
 
     display(context, program_state) {
         // Setup -- This part sets up the scene's overall camera matrix, projection matrix, and lights:
-        let camera_matrix = Mat4.identity().times(Mat4.translation(1,0,-5))
-            .times(Mat4.rotation(-45.25*Math.PI/2, 0, 1, 0))
-            .times(Mat4.translation( -.8, 0, 0));
+        let camera_matrix = Mat4.identity().times(Mat4.translation(1, 0, -6))
+            .times(Mat4.rotation(-45.25 * Math.PI / 2, 0, 1, 0))
+            .times(Mat4.translation(-.8, 0, 0));
 
         if (!context.scratchpad.controls) {
             this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
@@ -183,22 +160,47 @@ export class House extends Scene {                           // **Obj_File_Demo*
 
         super.display(context, program_state);
         let model_transform = Mat4.identity();
-        model_transform = model_transform.times(Mat4.scale(2,2,2));
+        model_transform = model_transform.times(Mat4.scale(1.5, 1.5, 1.5));
 
         const t = program_state.animation_time;
 
         program_state.projection_transform = Mat4.perspective(Math.PI / 4, context.width / context.height, 1, 500);
         // A spinning light to show off the bump map:
-        program_state.lights = [new Light(vec4(3, 2, 10, 1), color(1, 1, 1, 1), 100000)];
+        //program_state.lights = [new Light(vec4(3, 2, 10, 1), color(1, 1, 1, 1), 100000)];
+        program_state.lights = [new Light(
+            Mat4.rotation(t / 1000, 1, 0, 0).times(vec4(3, 2, 10, 1)),
+            color(1, 1, 1, 1), 100000)];
         //const light_position = vec4(10, 10, 10, 1); //moved point position to origin (0,0,0)
         //program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 1000)];
 
-        this.shapes.house.draw(context, program_state, model_transform,this.materials.house);
+        // house
+        this.shapes.house.draw(context, program_state, model_transform, this.materials.house);
+
+        // ground
+        let ground_transform = model_transform.times(Mat4.translation(0,-.6,0))
+            .times(Mat4.scale(5, 5, 5));
+        this.shapes.ground.draw(context, program_state, ground_transform, this.materials.house);
+
+        // bush
+        let bush_transform = model_transform.times(Mat4.translation(1,-.6,-.7))
+            .times(Mat4.scale(.25, .25, .25));
+        this.shapes.bush.draw(context, program_state, bush_transform, this.materials.house);
+
+        // leaves
+        let leaves_transform = model_transform.times(Mat4.translation(1.5,.7,1))
+            .times(Mat4.rotation(2 * Math.PI / 2, 1, 0, 0))
+            .times(Mat4.scale(.25, .25, .25));
+        this.shapes.leaves.draw(context, program_state, leaves_transform, this.materials.house);
+
+        // trunk
+        let trunk_transform = model_transform.times(Mat4.translation(1.5,0,1))
+            .times(Mat4.rotation(2 * Math.PI / 2, 1, 0, 0)).times(Mat4.scale(.7, .7, .7));
+        this.shapes.trunk.draw(context, program_state, trunk_transform, this.materials.house);
+
+        // stepstones
+        let stepstones_transform = model_transform.times(Mat4.translation(1.5,-.58,0))
+            .times(Mat4.scale(.3, .3, .3));
+        this.shapes.stepstones.draw(context, program_state, stepstones_transform, this.materials.house);
 
     }
 }
-//Define Main_Scene Class
-const Main_Scene = Plane;
-const Additional_Scenes = [];
-
-export {Main_Scene, Additional_Scenes, Canvas_Widget, Code_Widget, Text_Widget, defs}
